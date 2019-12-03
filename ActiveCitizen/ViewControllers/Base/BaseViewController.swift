0759054822cc4d85e9c786fb0501d6a84d365494
Dmitry Byankin <dmitry.byankin@antares-software.ru>
2019-12-03 15:43:42 +0300

    Add more view controllers

--- ActiveCitizen/ViewControllers/Base/BaseViewController.swift
+++ ActiveCitizen/ViewControllers/Base/BaseViewController.swift
@@ -32,19 +32,19 @@ class BaseViewController: UIViewController, UITextFieldDelegate, NVActivityIndic
     // @IBInspectable var keyboardManagment = false
     
     // TODO
-    /*
-    -(void)setKeyboardManagment:(BOOL)keyboardManagment {
-
-        if(_keyboardManagment != keyboardManagment){
-            if(keyboardManagment){
-                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-            }else{
-                [[NSNotificationCenter defaultCenter] removeObserver:self];
-            }
-            _keyboardManagment = keyboardManagment;
-        }
-    }*/
+    
+    //    -(void)setKeyboardManagment:(BOOL)keyboardManagment {
+    //
+    //        if(_keyboardManagment != keyboardManagment){
+    //            if(keyboardManagment){
+    //                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    //                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    //            }else{
+    //                [[NSNotificationCenter defaultCenter] removeObserver:self];
+    //            }
+    //            _keyboardManagment = keyboardManagment;
+    //        }
+    //    }
     
 
     /// Is view is stil appearing. (Beyound WillAppear and DidAppear)
